2515ee03348f3edbd11a4f25d2c479359cefd461
Andrew Studenic <36557385+AndrewStudenic@users.noreply.github.com>
2019-02-21 17:56:08 -0800

    Initial Commit

---
+++ MarioKart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarioKart
+//
+//  Created by ANDREW STUDENIC on 2/21/19.
+//  Copyright © 2019 ANDREW STUDENIC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
